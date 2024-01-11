908db2c56fe392f2dda2a7ff40d0b7526a600bcc
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-11 16:04:38 +0100

    Add project files.

---
+++ Kata1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Kata1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Is number square?: {IsSquare(input)}");
+        }
+        public static bool IsSquare(int n)
+        {
+            double sqrt = Math.Sqrt(n);
+
+            return (sqrt % 1 == 0) ? true : false;
+        }
+    }
+}
